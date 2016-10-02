e0898715a945593ee7cf476935f87ee096e08dd0
Xiaowen Fang <164426683@qq.com>
2016-10-01 20:49:12 -0400

    Update test_bookRecycle.js

--- test_bookRecycle.js
+++ test_bookRecycle.js
@@ -103,11 +103,13 @@ $( document ).ready(function() {
 	});
 	//React component for all book info under a courseID and school ID
 	var BookTable = React.createClass({
-		render: function() {
-			var bookNodes = this.props.data.map(function(book) {
-				return(<BookRow data={book} key={book.id}></BookRow>);
+		render: function(){
+			var bookNodes = this.props.data.map(function(book){
+				return(<BookRow data={book} key={book.id}>);
 			});
-			return {bookNodes};
+			
+			return ({bookNodes});
+			
 		}
 	});
 
@@ -117,11 +119,16 @@ $( document ).ready(function() {
 		var ref = new Firebase("https://bookrecycle-5b8d1.firebaseio.com/school/" + school + "/" + courseID);
 		console.log("find data!");
 		ref.once("value", function(snapshot) {
-				console.log("this is not json " + snapshot.exportVal());
+			
+			var jsonBook = [];
+			snapshot.forEach(function(child){
+				jsonBook.push(child);
+			})
+				console.log("this is jsonBook " + jsonBook);
 				
-				console.log("course info " + snapshot.val().author); //test if get the data
+				console.log("course info " + jsonBook[0].author); //test if get the data
 				//render the data
-				ReactDOM.render(<BookTable data={snapshot.exportVal()} />,
+				ReactDOM.render(<BookTable data={jsonBook} />,
 				document.getElementById('searchResultTable'));
 			
 			
