8348f822a8ee70e5c102cde531e950b1e0442226
Xiaowen Fang <164426683@qq.com>
2016-10-01 22:23:44 -0400

    Update test_bookRecycle.js

--- test_bookRecycle.js
+++ test_bookRecycle.js
@@ -126,58 +126,14 @@ $( document ).ready(function() {
 		ref.once("value", function(snapshot) {			
 		  	//go through each postingID of this school and course search combination
 			snapshot.forEach(function(childSnapshot) {
-				$('#postingsAppear').html('<b>Textbook postings for ' + courseID + ' at ' + school + ' are the following: ');
+				
 				
 				console.log('current postingID: '+ childSnapshot.key());
 				var postingID = childSnapshot.key();
 				
-				//add row at the bottom and insert cells
-				var row = table.insertRow(table.rows.length);	
-				var course = row.insertCell(0);
-				var textbookTitle = row.insertCell(1);
-				var isbn = row.insertCell(2);
-				var author = row.insertCell(3);
-				var sellerCell = row.insertCell(4);
-				var email = row.insertCell(5);
-				var phone = row.insertCell(6);
-				var notes = row.insertCell(7);
-				var price = row.insertCell(8);
 				
-				//get textbook info
-				course.innerHTML = courseID;
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/title").once('value').then(function(snapshot) {
-					textbookTitle.innerHTML = snapshot.val();
-				});
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/isbn").once('value').then(function(snapshot) {
-					isbn.innerHTML = snapshot.val();
-				});
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/author").once('value').then(function(snapshot) {
-					author.innerHTML = snapshot.val();
-				});
-				//get seller info (full name, email, phone)
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/sellerID").once('value').then(function(snapshot) {
-					var sellerID = snapshot.val();
-					firebase.database().ref("users/" + sellerID +"/firstName").once('value').then(function(snapshot) {
-						var firstname = snapshot.val();
-						firebase.database().ref("users/" + sellerID +"/lastName").once('value').then(function(snapshot) {
-							sellerCell.innerHTML = firstname + " " + snapshot.val();
-						});
-					});
-					firebase.database().ref("users/" + sellerID +"/email").once('value').then(function(snapshot) {
-						email.innerHTML = snapshot.val();
-					});
-					firebase.database().ref("users/" + sellerID +"/phone").once('value').then(function(snapshot) {
-						phone.innerHTML = snapshot.val();
-					});
-					
-				});
 				
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/note").once('value').then(function(snapshot) {
-					notes.innerHTML = snapshot.val();
-				});
-				firebase.database().ref("school/" + school + "/" + courseID + "/" + postingID + "/price").once('value').then(function(snapshot) {
-					price.innerHTML = snapshot.val();
-				});
+				
 			});
 		  $('#spinner').hide();	
 		});
