0231fda34c59bcdc5361bc8c51967490546d37db
mjmbauzon <mjmbauzon@yahoo.com>
2016-10-26 13:38:21 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+console.log('running 1st line');
 var firebase = require("firebase");
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
