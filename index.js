c213b424a8602423437160a2d4ae2010bfdda75a
mjmbauzon <mjmbauzon@yahoo.com>
2016-10-26 13:39:23 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log('running 1st line');
-var firebase = require("firebase");
+//var firebase = require("firebase");
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
@@ -7,11 +7,11 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
-
+/*
 firebase.initializeApp({
     serviceAccount: "privkey.json",
     databaseURL: "https://bookrecycle-5b8d1.firebaseio.com/"
-});
+});*/
 //var fireRef = firebase.database().ref('school');
 
 var port = process.env.PORT || 3000;
