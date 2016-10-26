bf3f17750d739e225f78e60bd5f0403acb949652
mjmbauzon <mjmbauzon@yahoo.com>
2016-10-26 15:06:13 -0400

    update index.js

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ console.log('running 1st line');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('/index');
 });
 /*
 firebase.initializeApp({
